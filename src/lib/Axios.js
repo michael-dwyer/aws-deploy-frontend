b344b2d3a2dadfe37cccf73da194b5128c3536fe
Michael Todd Dwyer <michael.todd.dwyer@gmail.com>
2020-04-21 22:58:34 -0400

    /api

--- src/lib/Axios.js
+++ src/lib/Axios.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://http://ec2-3-83-30-240.compute-1.amazonaws.com/api' : '',
-    timeout: 50000
+    baseURL: process.env.NODE_ENV === 'development' ? '/api' : '',
+    timeout: 50000,
+    crossdomain: true,
+    headers: {
+        "Access-Control-Allow-Origin" : "*"
+    }
 })
 
 export default Axios;
