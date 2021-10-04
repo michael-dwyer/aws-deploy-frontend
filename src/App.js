459b7a066f0873c1fca84837193b09bddc63ad8c
mtdwyer <michael.todd.dwyer@gmail.com>
2021-10-04 10:34:34 -0400

    updated package and node env variable

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <>  
+      <> 
         <Navbar />
         <Switch>
           <Route exact path='/' component={Home} />
